a6a36930a18e84c1f24887314d617113b0af39bd
Jim Raynor <jimraynor.sko@gmail.com>
2014-02-17 22:33:41 +1100

    command update wip

--- dndtools/MircDndToolkit/src/org/toj/dnd/irctoolkit/engine/command/game/D6sDiceRollCommand.java
+++ dndtools/MircDndToolkit/src/org/toj/dnd/irctoolkit/engine/command/game/D6sDiceRollCommand.java
@@ -4,19 +4,28 @@ import java.util.List;
 
 import org.toj.dnd.irctoolkit.dice.D6sRoll;
 import org.toj.dnd.irctoolkit.engine.command.GameCommand;
+import org.toj.dnd.irctoolkit.engine.command.IrcCommand;
+import org.toj.dnd.irctoolkit.engine.command.IrcCommand.CommandSegment;
 import org.toj.dnd.irctoolkit.exceptions.ToolkitCommandException;
 import org.toj.dnd.irctoolkit.io.udp.OutgoingMsg;
 import org.toj.dnd.irctoolkit.token.Color;
 import org.toj.dnd.irctoolkit.util.IrcColoringUtil;
 
+@IrcCommand(command = "d6s", args = { CommandSegment.INT, CommandSegment.NULLABLE_LIST } )
 public class D6sDiceRollCommand extends GameCommand {
 
     private int diceNumber;
     private String[] desc;
 
-    public D6sDiceRollCommand(int diceNumber, String[] desc) {
-        this.diceNumber = diceNumber;
-        this.desc = desc;
+    public D6sDiceRollCommand(Object[] args) {
+        diceNumber = (Integer) args[0];
+
+        if (args.length == 1) {
+            desc = new String[0];
+        } else {
+            desc = new String[args.length - 1];
+            System.arraycopy(args, 1, desc, 0, desc.length);
+        }
     }
 
 
