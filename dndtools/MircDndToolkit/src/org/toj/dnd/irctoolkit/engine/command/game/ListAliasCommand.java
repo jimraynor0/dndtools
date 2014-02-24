c81a70f978b191c7a18155d09bb7f3ad7bf3bfb6
Jim Raynor <jimraynor.sko@gmail.com>
2014-02-24 20:32:28 +1100

    more 3e compatible

---
+++ dndtools/MircDndToolkit/src/org/toj/dnd/irctoolkit/engine/command/game/ListAliasCommand.java
@@ -0,0 +1,23 @@
+package org.toj.dnd.irctoolkit.engine.command.game;
+
+import java.util.Map;
+
+import org.toj.dnd.irctoolkit.engine.command.IrcCommand;
+import org.toj.dnd.irctoolkit.engine.command.UndoableTopicCommand;
+import org.toj.dnd.irctoolkit.exceptions.ToolkitCommandException;
+
+@IrcCommand(command="listalias", args = {})
+public class ListAliasCommand extends UndoableTopicCommand {
+
+    public ListAliasCommand(Object[] args) {
+        super();
+    }
+
+    @Override
+    public void doProcess() throws ToolkitCommandException {
+        Map<String, String> map = getGame().getAliases();
+        for (String alias : map.keySet()) {
+            sendMsg(alias + " => " + map.get(alias));
+        }
+    }
+}
