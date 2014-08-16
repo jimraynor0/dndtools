6e0f6d7861bde8e48d9490745c7e10ee82050b2d
JimRaynor <JimRaynor@gmail.com>
2014-08-16 22:43:59 +1000

    add random trap command

---
+++ dndtools/MircDndToolkit/src/org/toj/dnd/irctoolkit/engine/command/map/PlaceRandomTrapCommand.java
@@ -0,0 +1,89 @@
+package org.toj.dnd.irctoolkit.engine.command.map;
+
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
+import org.toj.dnd.irctoolkit.engine.ToolkitEngine;
+import org.toj.dnd.irctoolkit.engine.command.IrcCommand;
+import org.toj.dnd.irctoolkit.engine.command.IrcCommand.CommandSegment;
+import org.toj.dnd.irctoolkit.engine.command.MapCommand;
+import org.toj.dnd.irctoolkit.exceptions.ToolkitCommandException;
+import org.toj.dnd.irctoolkit.filter.MapFilter;
+import org.toj.dnd.irctoolkit.map.MapModel;
+import org.toj.dnd.irctoolkit.map.MapObject;
+import org.toj.dnd.irctoolkit.map.Point;
+import org.toj.dnd.irctoolkit.token.Color;
+import org.toj.dnd.irctoolkit.util.TrapUtil;
+
+@IrcCommand(command="randomtraps", args = {CommandSegment.INT, CommandSegment.INT})
+public class PlaceRandomTrapCommand extends MapCommand {
+
+    private int numberOfTraps;
+    private int trapCr;
+
+    public PlaceRandomTrapCommand(Object[] args) {
+        numberOfTraps = (Integer) args[0];
+        trapCr = (Integer) args[1];
+    }
+
+    @Override
+    protected void doExecute() throws ToolkitCommandException {
+        StringBuilder filterParam = new StringBuilder();
+
+        for (int i = 0; i < numberOfTraps; i++) {
+            Point p = findAvailablePosition();
+            MapModel model;
+            try {
+                model = generateTrapModel(i + 1);
+                context.getModelList().add(model);
+                context.getCurrentMap().drawObjectsOntoGrid(new int[] {p.getX()}, new int[] {p.getY()}, model);
+                if (filterParam.length() > 0) {
+                    filterParam.append(",");
+                }
+                filterParam.append(model.getId());
+            } catch (IOException e) {
+                throw new ToolkitCommandException(e);
+            }
+        }
+        MapFilter filter = MapFilter.MapFilterFactory.createFilter(MapFilter.TYPE_INVISIBILITY_FILTER, filterParam.toString());
+        filter.setActive(true);
+        context.getFilterList().add(filter);
+    }
+
+    private MapModel generateTrapModel(int i) throws IOException {
+        MapModel model = new MapModel();
+        model.setCh("T" + i);
+        model.setDesc(getRandomTrapByLevel().name);
+        model.setForeground(Color.BLACK);
+        return model;
+    }
+
+    private TrapUtil.Trap getRandomTrapByLevel() throws IOException {
+        return TrapUtil.getRandomTrap(trapCr);
+    }
+
+    private Point findAvailablePosition() {
+        int x = (int) (Math.random() * context.getCurrentMap().getWidth());
+        int y = (int) (Math.random() * context.getCurrentMap().getHeight());
+        MapObject[] objsAtPos = context.getCurrentMap().findObjAt(x, x, y, y);
+        if (objsAtPos != null && objsAtPos.length > 0) {
+            return findAvailablePosition();
+        }
+        return new Point(x, y);
+    }
+
+    @Override
+    protected boolean mapChanged() {
+        return true;
+    }
+
+    @Override
+    protected boolean modelChanged() {
+        return true;
+    }
+
+    @Override
+    protected boolean filterChanged() {
+        return true;
+    }
+}
---
+++ dndtools/MircDndToolkit/src/org/toj/dnd/irctoolkit/util/TrapUtil.java
@@ -0,0 +1,72 @@
+package org.toj.dnd.irctoolkit.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.toj.common.FileIoUtils;
+import org.toj.dnd.irctoolkit.dice.Dice;
+
+public class TrapUtil {
+    private static final File TRAPS_FILE = new File("./resources/traps.config");
+    private static Map<Integer, List<Trap>> ALL_TRAPS;
+
+    public static Trap getRandomTrap(int cr) throws IOException {
+        List<Trap> traps = getTrapList(cr);
+        Trap trap = traps.get(Dice.getDice(traps.size()).roll() - 1);
+        return trap;
+    }
+
+    public static List<Trap> getTrapList(int cr) throws IOException {
+        return loadTrapListFromFile().get(cr);
+    }
+
+    private static Map<Integer, List<Trap>> loadTrapListFromFile()
+        throws IOException {
+        if (ALL_TRAPS == null) {
+            ALL_TRAPS = new HashMap<Integer, List<Trap>>();
+            BufferedReader reader = FileIoUtils.getReader(TRAPS_FILE);
+            String line = null;
+
+            String name = null;
+            String desc = null;
+            while ((line = reader.readLine()) != null) {
+                if (line == null || line.isEmpty()) {
+                    if (name == null || desc == null) {
+                        continue;
+                    }
+
+                    Trap trap = new Trap();
+                    trap.name = name;
+                    trap.desc = desc;
+
+                    String crStr = desc.split(";")[0];
+                    int cr = Integer.parseInt(crStr.substring(3).trim());
+                    
+                    if (!ALL_TRAPS.containsKey(cr)) {
+                        ALL_TRAPS.put(cr, new LinkedList<Trap>());
+                    }
+                    ALL_TRAPS.get(cr).add(trap);
+
+                    name = null;
+                    desc = null;
+                } else if (name == null) {
+                    name = line;
+                } else if (desc == null) {
+                    desc = line;
+                }
+            }
+            reader.close();
+        }
+        return ALL_TRAPS;
+    }
+
+    public static class Trap {
+        public String name;
+        public String desc;
+    }
+}
