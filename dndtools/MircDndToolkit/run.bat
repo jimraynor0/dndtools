4844d79292df8c8084846f658cec0c8ab73d90b7
JimRaynor <JimRaynor@gmail.com>
2014-11-15 19:56:12 +1100

    #10 fix alias WIP

--- dndtools/MircDndToolkit/run.bat
+++ dndtools/MircDndToolkit/run.bat
@@ -1,2 +1,2 @@
 @echo off
-start javaw -cp "MircDndToolkit-0.0.1-SNAPSHOT.jar;lib/*" org.toj.dnd.irctoolkit.engine.ToolkitLauncher
+start javaw -cp "jimstoolbox.jar;lib/*" org.toj.dnd.irctoolkit.engine.ToolkitLauncher
