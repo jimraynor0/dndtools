e3f41c36f50a0f5a6449348281e15a79a7abebd1
JimRaynor <JimRaynor@gmail.com>
2014-12-30 22:15:43 +1100

    #15 prettify the end turn message

--- dndtools/MircDndToolkit/src/org/toj/dnd/irctoolkit/engine/command/game/d6smw/EndCommand.java
+++ dndtools/MircDndToolkit/src/org/toj/dnd/irctoolkit/engine/command/game/d6smw/EndCommand.java
@@ -2,6 +2,8 @@ package org.toj.dnd.irctoolkit.engine.command.game.d6smw;
 
 import org.toj.dnd.irctoolkit.engine.command.IrcCommand;
 import org.toj.dnd.irctoolkit.exceptions.ToolkitCommandException;
+import org.toj.dnd.irctoolkit.token.Color;
+import org.toj.dnd.irctoolkit.util.IrcColoringUtil;
 
 @IrcCommand(command = "end", args = {})
 public class EndCommand extends D6smwGameCommand {
@@ -22,6 +24,6 @@ public class EndCommand extends D6smwGameCommand {
         getGame().end();
         sendTopic(getGame().generateTopic());
         refreshTopic();
-        sendMsg("轮到" + getGame().getCurrent().getName() + "行动了");
+        sendMsg("轮到" + IrcColoringUtil.paint(getGame().getCurrent().getName(), Color.PURPLE.getCode()) + "行动了");
     }
 }
